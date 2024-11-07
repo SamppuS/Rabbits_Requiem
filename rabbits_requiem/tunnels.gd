ab27c16905783e1299d30770d934e098763f1651
iroguin <143116756+Iroguin@users.noreply.github.com>
2024-11-07 19:29:41 +0200

    added starting point to signal

--- rabbits_requiem/tunnels.gd
+++ rabbits_requiem/tunnels.gd
@@ -6,7 +6,7 @@ func _ready():
 	var minimap = get_node("Minimap")
 	minimap.grid_updated.connect(_on_grid_received)
 
-func _on_grid_received(new_grid):
+func _on_grid_received(new_grid, sp):
 	received_grid = new_grid
 	# Now you can use the received grid data
 	print("Received new grid data: ", received_grid)
